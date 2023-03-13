84162857569a8cadd9abb47bd6e1045e1c5d9d25
Shahid Gillani <shahid5ssg@gmail.com>
2023-03-13 23:49:35 +0500

    Deploying front end on netlify

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -58,7 +58,7 @@ function App() {
 
       //save user to MongoDB
       if(profileObj){
-        const response=await fetch('http://localhost:8080/api/v1/users',{
+        const response=await fetch('https://ill-mite-uniform.cyclic.app/api/v1/users',{
           method:'POST',
           headers:{'Content-Type' : 'application/json'},
           body:JSON.stringify({
@@ -132,7 +132,7 @@ function App() {
         <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
         <RefineSnackbarProvider>
           <Refine
-            dataProvider={dataProvider("http://localhost:8080/api/v1")}
+            dataProvider={dataProvider("https://ill-mite-uniform.cyclic.app/api/v1")}
             notificationProvider={notificationProvider}
             ReadyPage={ReadyPage}
             catchAll={<ErrorComponent />}
